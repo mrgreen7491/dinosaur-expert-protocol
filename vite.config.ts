cac24ce73b4a62b799308a9b4bf0146349e83897
mrgreen7491 <mrgreen7491@gmail.com>
2026-07-02 09:54:38 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/dinosaur-expert-protocol/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
